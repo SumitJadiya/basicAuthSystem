b1fbb13b797679582edd5736ce9bd71b90f4f229
Sumit Jadiya <jadiyaskj@gmail.com>
2021-12-08 22:12:32 +0530

    add model

---
+++ model/user.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  firstName:  {
+      type: String,
+      default: null
+    },
+    lastName:  {
+      type: String,
+      default: null
+    },
+    email:  {
+      type: String,
+      unique: true
+    },
+  country:   String,
+  password:   String,
+  token:   String,
+});
+
+module.exports = mongoose.model('User', userSchema)
